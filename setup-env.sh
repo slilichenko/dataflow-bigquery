ec99dcd22f9d0352657c7c9b2c732aa6120acdbd
Sergei Lilichenko <slilichenko@google.com>
2021-10-26 15:45:10 -0700

    Updated the terraform scripts to handle the output variables correctly.

--- setup-env.sh
+++ setup-env.sh
@@ -8,12 +8,12 @@ export TF_VAR_bigquery_dataset_location=${BIGQUERY_REGION}
 cd terraform
 terraform init && terraform apply
 
-export EVENT_GENERATOR_TEMPLATE=$(terraform output event-generator-template)
-export EVENT_TOPIC=$(terraform output event-topic)
-export EVENT_SUB=$(terraform output event-sub)
-export DATASET=$(terraform output bq-dataset)
-export DATAFLOW_TEMP_BUCKET=gs://$(terraform output dataflow-temp-bucket)
-export DATA_BUCKET=gs://$(terraform output data-bucket)
-export REGION=$(terraform output region)
+export EVENT_GENERATOR_TEMPLATE=$(terraform output -raw event-generator-template)
+export EVENT_TOPIC=$(terraform output -raw event-topic)
+export EVENT_SUB=$(terraform output -raw event-sub)
+export DATASET=$(terraform output -raw bq-dataset)
+export DATAFLOW_TEMP_BUCKET=gs://$(terraform output -raw dataflow-temp-bucket)
+export DATA_BUCKET=gs://$(terraform output -raw data-bucket)
+export REGION=$(terraform output -raw region)
 
 cd ..
